319011cacc4b239419a2e979dda0acb4dd2d4781
Leo <ai410320@gmail.com>
2020-04-02 13:16:01 +0800

    originFile

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ import Home from '@/components/HelloWorld' ;
 import Page from '@/components/pages/page' ;
 // ＠ 為 src , 上方為自定義的分頁元件
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 //此配置文件需要可以匯出給 entry (main.js) 使用
 export default new VueRouter({
@@ -20,6 +20,6 @@ export default new VueRouter({
             name: '分頁', // 元件接下來會呈現的名稱
             path: '/page', //元件的虛擬路徑 (在網址上輸入的路徑)
             component: Page, //對應的元件
-        },
+        }
     ]
 });
